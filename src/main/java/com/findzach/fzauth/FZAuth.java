0b277f606917aca4065620f72ef7b114f7ceeadf
FindZach <zach@findzach.com>
2023-09-02 23:33:40 -0400

    init commit of FZAuth - MC Simple Auth Plugin

---
+++ src/main/java/com/findzach/fzauth/FZAuth.java
@@ -0,0 +1,17 @@
+package com.findzach.fzauth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FZAuth extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
